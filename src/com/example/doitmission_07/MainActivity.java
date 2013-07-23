a8e7c523751cca4e5a9a9fdb62a54484f52100af
MW KIM <infoconlv5@gmail.com>
2013-07-23 15:49:17 +0900

    commit two

--- src/com/example/doitmission_07/MainActivity.java
+++ src/com/example/doitmission_07/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.doitmission_07;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
 
+	GridView monthView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		monthView = (GridView) findViewById(R.id.monthView);
+		
+		monthText = (TextView) findViewById(R.id.monthText);
+		setMonthText();
 	}
 
 	@Override
@@ -17,6 +25,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private void setMonthText() {
+		curYear = monthViewAdapter.getCurYear();
+		curMonth = monthViewAdapter.getCurMonth();
 	}
 
 }
